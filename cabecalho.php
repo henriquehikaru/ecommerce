0fe1e4ffac99afbc00927d47ef6a9e034ee9f834
henriquehikaru <henrique.hikaru@unesp.br>
2025-09-03 21:36:03 -0300

    moving from cabecalho condition to print to index

--- cabecalho.php
+++ cabecalho.php
@@ -9,18 +9,4 @@
     else {
         $sessaoConectado = false;
     }
-
-    if ( $sessaoConectado ) {
-        $idSessao = session_id();
-        echo "<a href='logout.php'>Sair</a>
-            <br>Olá, $login (ID sessão:<b>$idSessao</b>)
-            <br>
-            <a href='produtos.php'>Produtos</a>
-            <a href='usuarios.php'>Usuarios</a>";
-    }
-    else {
-        echo "<a href='login.php'></a>";
-    }
-    echo "<hr>";
-
 ?>
--- index.php
+++ index.php
@@ -3,9 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include("cabecalho.php"); ?>
     <title>Home</title>
 </head>
 <body>
+    <?php 
+        if ( $sessaoConectado ) {
+        $idSessao = session_id();
+        echo "<a href='logout.php'>Sair</a>
+            <br>Olá, $login (ID sessão:<b>$idSessao</b>)
+            <br>
+            <a href='produtos.php'>Produtos</a>
+            <a href='usuarios.php'>Usuarios</a>";
+}
+        else {
+            echo "<a href='login.php'></a>";
+        }
+        echo "<hr>";    
+
+    ?>
     
 </body>
 </html>
